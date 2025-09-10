76d09d1f7978d7ffc94f1397b35080dc9869cad8
Julien Polo <julien.polo@inthememory.com>
2025-09-10 18:51:34 +0200

    📝 Add comment for LAPTOP_PROFILE

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@
 #
 RELEASE="main"
 REPOSITORY_NAME=${REPOSITORY_NAME:-"Inthememory/laptop"}
+# ⬇ Uncomment this if you want to force default install profile
+# export LAPTOP_PROFILE="default"
 export LAPTOP_GIT_REMOTE="https://github.com/$REPOSITORY_NAME.git"
 
 #==============================================================================
