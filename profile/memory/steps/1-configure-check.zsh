e141341810a8f4d18b0201b0875ed6f5b4b130a2
Julien Polo <julien.polo@gmail.com>
2025-12-19 17:48:02 +0100

    🐛 Fix missing method laptop_warn and laptop_info

--- profile/memory/steps/1-configure-check.zsh
+++ profile/memory/steps/1-configure-check.zsh
@@ -2,7 +2,7 @@
 source "$LAPTOP_HOME/lib/env.sh"
 
 laptop_ssh_test_key "git@github.com" \
-  && laptop_info "🔑✅ SSH valid on github.com." \
-  || laptop_warn "🔑❌ SSH invalid on github.com. Please register on https://github.com/settings/keys"
+  && laptop_log info "🔑✅ SSH valid on github.com." \
+  || laptop_log warn "🔑❌ SSH invalid on github.com. Please register on https://github.com/settings/keys"
 
 laptop_step_finished
