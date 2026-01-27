6d0d3ae2106be426ff1bfafbf074d2753fda223f
Julien Polo <julien.polo@gmail.com>
2026-01-27 18:30:01 +0100

    🐛 Escape special characters to avoid command execution

--- profile/memory/steps/0-configure-all.zsh
+++ profile/memory/steps/0-configure-all.zsh
@@ -31,7 +31,7 @@ if [ "$LAPTOP_DEVCONTAINER" = "false" ];then
   laptop_ssh_ensure_setting "Host *" "IdentityFile" "~/.ssh/id_ed25519"
 
   # Setup github token for memory packages
-  laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN_MEMORY_PACKAGES" '"$(gh auth token 2>/dev/null)"'
+  laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN_MEMORY_PACKAGES" '\"\$(gh auth token 2>/dev/null)\"'
 fi
 
 laptop_package_ensure "profile:core"
