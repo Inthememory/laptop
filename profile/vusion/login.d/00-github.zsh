f3f27581a900882afa1ae630473d1f71a89fd9bd
Julien Polo <julien.polo@vusion.com>
2026-07-21 17:35:39 +0200

    🔧 Update GitHub login scope to allow write access for memory packages and remove unused .keep file

--- profile/vusion/login.d/00-github.zsh
+++ profile/vusion/login.d/00-github.zsh
@@ -4,7 +4,7 @@
 # Uses the existing `laptop_github_ensure_login` function.
 
 # scope is required to use the github token for memory packages
-laptop_github_ensure_login --scopes "read:packages"
+laptop_github_ensure_login --scopes "write:packages"
 
 # Setup github token for memory packages
 laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN" '"$(GITHUB_TOKEN= gh auth token 2>/dev/null)"' --export
