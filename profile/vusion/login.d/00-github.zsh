37314160fbff6355f82fec4a28d92098e6b62bd6
Julien Polo <julien.polo@vusion.com>
2026-07-21 17:37:02 +0200

    🔧 Add GitHub token configuration for npm authentication

--- profile/vusion/login.d/00-github.zsh
+++ profile/vusion/login.d/00-github.zsh
@@ -9,3 +9,6 @@ laptop_github_ensure_login --scopes "write:packages"
 # Setup github token for memory packages
 laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN" '"$(GITHUB_TOKEN= gh auth token 2>/dev/null)"' --export
 laptop_shell_ensure_var "$HOME/.profile" "GITHUB_TOKEN_MEMORY_PACKAGES" '"${GITHUB_TOKEN}"' --export
+
+# Setup github token for npm
+laptop_npm_config_ensure "//npm.pkg.github.com/:_authToken" '${GITHUB_TOKEN}'
